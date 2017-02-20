18f6889ae94bb635b1a5145ae8c99a518f7a7969
Karpov Babu <karpovbabu.v@gmail.com>
2017-02-20 16:04:45 +0530

    ListController
    Recreated List Controller and Dropdown Html

--- src/main/resources/static/js/listController.js
+++ src/main/resources/static/js/listController.js
@@ -1,29 +1,17 @@
-mainApp
-		.controller('ListController', function($scope, SiteService) {
-			$scope.getID = null;
-			$scope.Result1 = "";
 
-			SiteService.GetInfo().then(function(d) {
-				$scope.SiteList = d.data; // success
+
+mainApp.controller('ListController', function($scope, $http) {
+	$http.get("http://localhost:999/MegatechEnterprise/site/getSite").then(
+			function(response) {
+				$scope.SiteList = response.data; // response data
+
 			}, function(error) {
 				alert('Error!');
 			});
-			
-			
-			$scope.ShowArea = function() {
-				$scope.Result1 = "Selected Country ID";
-			}
+	
+	  $scope.doSomething = function(clickedId){
+		  alert("Clicked Id is "+clickedId);
+		  console.log("Clicked Id is "+clickedId)
+	  };
 
-		})
-		.factory(
-				'SiteService',
-				function($http) { // factory methos to get data from server.
-					var fac = {};
-					fac.GetInfo = function() {
-						return $http
-								.get('http://localhost:999/MegatechEnterprise/site/getSite')
-					}
-
-					return fac;
-				});
-
+});
