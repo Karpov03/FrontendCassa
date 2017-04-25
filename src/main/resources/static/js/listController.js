32c62c1d563108d3fae150952944db1d3bffbdeb
Karpov Babu <karpovbabu.v@gmail.com>
2017-04-25 17:37:12 +0530

    All Controller js combined

--- src/main/resources/static/js/listController.js
+++ src/main/resources/static/js/listController.js
@@ -1,178 +1,379 @@
-//mainApp.config([ '$locationProvider', function($locationProvider) {
-//	$locationProvider.html5Mode(true).hashPrefix("");
-//} ]);
+mainApp.run(function($rootScope) {
 
-mainApp.config([ "$stateProvider", "$urlRouterProvider", '$locationProvider',
-		function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $rootScope.myDateRange = {};
+    $rootScope.myDateRange.startDate = new Date().getTime() - 43200000;
+    $rootScope.myDateRange.endDate = new Date().getTime();
 
-			$urlRouterProvider.otherwise("/home/15");
+});
 
-			$stateProvider.state("home", {
 
-				url : "/home/:id",
-				templateUrl : 'views/home-content.html',
-				controller : 'RouteController',
-				params : {
-					id : '15'
-				}
 
-			}).state("login", {
+mainApp.config(["$stateProvider", "$urlRouterProvider", '$locationProvider',
+    function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-				url : "/login",
-				templateUrl : 'login.html',
-				controller : 'RouteController',
+        $urlRouterProvider.otherwise("/home/15");
 
-			}).state("area", {
+        $stateProvider.state("home", {
 
-				url : "/area/:id",
-				templateUrl : 'views/site-content.html',
-				controller : 'RouteController',
-				// default uri params
-				params : {
-					id : '15'
-				}
+            url: "/home/:id",
+            templateUrl: 'views/home-content.html',
+            controller: 'RouteController',
+            params: {
+                id: '15'
+            }
 
-			});
-			$locationProvider.html5Mode(true);
+        }).state("login", {
 
-		} ]);
+            url: "/login",
+            templateUrl: 'login.html',
+            controller: 'RouteController',
 
-/*
- * mainApp.config(function($routeProvider) { $routeProvider.when('/home', {
- * templateUrl : 'views/content.html', controller : 'RouteController'
- * }).when('/area/RM3-A (Floor)', { templateUrl : 'views/content.html',
- * controller : 'RouteController' }).otherwise({ redirectTo : '/' }); });
- */
+        }).state("area", {
 
-/*
- * mainApp.controller('ChartController', function($scope, $http) { $scope.config = {
- * title : 'Products', tooltips : true, labels : true, mouseover : function() { },
- * mouseout : function() { }, click : function() { }, legend : { display : true, //
- * could be 'left, right' position : 'right' } };
- * 
- * var chdata = $http.get(
- * "https://megatechenterprise.mybluemix.net/site/getSite").then(
- * function(response) { return response.data.value; // response data },
- * function(error) { alert('Error!'); });
- * 
- * $scope.data = { series : [ 'Sales', 'Income', 'Expense', 'Laptops',
- * 'Keyboards' ], data : [ { x : "Laptops", y : [ 200, 300 ] }, { x :
- * "Desktops", y : [ 300, 100, 100 ] }, { x : "Mobiles", y : [ 351 ] }, { x :
- * "Tablets", y : [ 54, 0, 879 ] } ] }; });
- */
+            url: "/area/:id",
+            templateUrl: 'views/site-content.html',
+            controller: 'RouteController',
+            // default uri params
+            params: {
+                id: '15'
+            }
+
+        });
+        $locationProvider.html5Mode(true);
+
+    }
+]);
 
 mainApp
-		.controller(
-				'RouteController',
-				[
-						'$scope',
-						'$http',
-						'$rootScope',
-						'$stateParams',
-						'$interval',
-						function($scope, $http, $rootScope, $stateParams,
-								$interval) {
-							// Make an AJAX call, retrieving the state.
+    .controller(
+        'RouteController', [
+            '$scope',
+            '$http',
+            '$rootScope',
+            '$stateParams',
+            '$interval',
+            function($scope, $http, $rootScope, $stateParams,
+                $interval) {
 
-							/*
-							 * $http .get(
-							 * "https://energyiotbackdev.mybluemix.net/tags/getlist")
-							 * .then(function(response) { $scope.ClickedList1 =
-							 * response.data;// response // data },
-							 * function(error) { // alert('Error!'); });
-							 */
+                var today = new Date().getTime();
+                var yesterday = new Date().getTime() - 43200000;
+                $scope.startDate = new Date().getTime() - 43200000;
+                $scope.endDate = new Date().getTime();
 
-							var resetdate = function() {
+                console.log(today + " : " + yesterday);
+                
+                var sid = $stateParams.id;
+                var sid1 = parseInt(sid) + parseInt(1);
+                var sid2 = parseInt(sid) + parseInt(2);
 
-								$rootScope.myDateRange.startDate = new Date()
-										.getTime() - 43200000;
-								$rootScope.myDateRange.endDate = new Date()
-										.getTime();
+                console.log("Defined id:" + sid1);
 
-							}
+                $scope.ranges = {
+                    "Today": [
+                        "2017-04-21T08:16:38.824Z",
+                        "2017-04-21T08:16:38.824Z"
+                    ],
+                    "Yesterday": [
+                        "2017-04-20T08:16:38.824Z",
+                        "2017-04-20T08:16:38.824Z"
+                    ],
+                    "Last 7 Days": [
+                        "2017-04-15T08:16:38.824Z",
+                        "2017-04-21T08:16:38.824Z"
+                    ],
+                    "Last 30 Days": [
+                        "2017-03-23T08:16:38.824Z",
+                        "2017-04-21T08:16:38.824Z"
+                    ],
+                    "This Month": [
+                        "2017-03-31T18:30:00.000Z",
+                        "2017-04-30T18:29:59.999Z"
+                    ],
+                    "Last Month": [
+                        "2017-02-28T18:30:00.000Z",
+                        "2017-03-31T18:29:59.999Z"
+                    ]
+                };
 
-							var id = $stateParams.id;
 
-							// console.log("Defined id:" +id);
 
-							var auto = $interval(function() {
-								$scope.gettag15();
-								$scope.gettag16();
-								$scope.gettag17();
-								}, 10000);
+                $scope.getDate = function(sDate, eDate) {
 
-							$scope.gettag15 = function() {
+                	
+                	   console.log("Defined id:" + sid);
+                	$scope.startDate = new Date(sDate).getTime();
+                	$scope.endDate = new Date(eDate).getTime();
 
-								resetdate();
-								$http
-										.get(
-												"https://energyiotbackdev.mybluemix.net/logics/sum/"
-														+ id
-														+ "/"
-														+ $rootScope.myDateRange.startDate
-														+ "/"
-														+ $rootScope.myDateRange.endDate)
-										.then(
-												function(response) {
-													$rootScope.myDateRange.tagValue15 = response.data;
-													$rootScope.myDateRange.tagValue15 = $rootScope.myDateRange.tagValue15 ;
+                    console.log("Selected Date is " + startDate +
+                        " : " + endDate)
+                        
+                        
+                        $scope.getAreaCharts($scope.startDate,$scope.endDate);
 
-													//console.log("Avg Date is "+ $rootScope.myDateRange.tagValue15)
-												}, function(error) {
-													// alert('Error!');
-												});
+                    $http.get(
+                        "https://energyiotbackdev.mybluemix.net/logics/sum/"+sid+"/" +
+                        $scope.startDate + "/" +
+                        $scope.endDate).then(
+                        function(response) {
+                          $scope.tagValue15 = response.data;
+                          
 
-							};
+                            //console.log("Avg Date is "	+ $rootScope.myDateRange.tagValue15)
 
-							$scope.gettag16 = function() {
-								$http
-										.get(
-												"https://energyiotbackdev.mybluemix.net/logics/sum/16/"
-														+ $rootScope.myDateRange.startDate
-														+ "/"
-														+ $rootScope.myDateRange.endDate)
-										.then(
-												function(response) {
-													$rootScope.myDateRange.tagValue16 = response.data;
-													$rootScope.myDateRange.tagValue16 = $rootScope.myDateRange.tagValue16;
-													//console.log("Avg Date is "	+ $rootScope.myDateRange.tagValue16)
-												}, function(error) {
-													// alert('Error!');
-												});
+                        },
+                        function(error) {
+                            // alert('Error!');
+                        });
 
-							};
+                }
+                // $scope.datePicker.date = {startDate: null, endDate: null};
 
-							$scope.gettag17 = function() {
-								$http
-										.get(
-												"https://energyiotbackdev.mybluemix.net/logics/sum/17/"
-														+ $rootScope.myDateRange.startDate
-														+ "/"
-														+ $rootScope.myDateRange.endDate)
-										.then(
-												function(response) {
-													$rootScope.myDateRange.tagValue17 = response.data;
-													$rootScope.myDateRange.tagValue17 = $rootScope.myDateRange.tagValue17;
-													//console.log("Avg Date is "+ $rootScope.myDateRange.tagValue17)
-												}, function(error) {
-													// alert('Error!');
-												});
 
-							};
 
-						}
+                $scope.chart1_label = [];
+                var chart1_tempdata = [];
+                //$scope.chart1_series = [ 'Series A', 'SeriesB' ];
+                $scope.chart1_data = [];
 
-				]);
+                $scope.chart1_colours = [{ // default
+                    "fillColor": "rgba(151,211,78,.6)",
+                    "strokeColor": "rgba(151,211,98,.7)",
+                    "pointColor": "rgba(151,211,58,.2)",
+                    "pointStrokeColor": "rgba(151,211,108,.3)",
+                    "pointHighlightFill": "rgba(151,211,128,.5)",
+                    "pointHighlightStroke": "rgba(151,211,128,.7)"
+                }];
+
+                $scope.options = {
+                    scales: {
+                        yAxes: [{
+                            id: 'y-axis-1',
+                            type: 'linear',
+                            display: true,
+                            position: 'left'
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    },
+                    responsive: false
+                }
+
+                $scope.chart2_label = ['AL WAHDAH', 'AL ROWDAH', 'AL MUSHRIF'];
+                var chart2_tempdata = [];
+                var chart2_tempdata1 = [];
+
+                $scope.chart2_data = [];
+                
+
+                $scope.chart2_colours = [{ // default
+                    "fillColor": "rgba(151,211,78,.8)",
+                    "strokeColor": "rgba(151,211,98,.7)"
+                }, { // default
+                    "fillColor": "rgba(53,162,203,.8)",
+                    "strokeColor": "rgba(53,162,273,.7)"
+                }];
+
+                function addZero(i) {
+                    if (i < 10) {
+                        i = "0" + i;
+                    }
+                    return i;
+                }
+
+
+
+                $scope.getAreaCharts = function(sDate, eDate) {
+
+                    startCDate = new Date(sDate).getTime();
+                    endCDate = new Date(eDate).getTime();
+                    $scope.chart1_label=[];
+                    $scope.chart1_data=[];
+
+                    $http.get(
+                        "https://energyiotbackdev.mybluemix.net/tags/gettag/" + sid + "/" +
+                       startCDate + "/" +
+                      endCDate).then(function(response) {
+
+                        angular.forEach(response.data, function(data) {
+                            var d = new Date(data.timestamps);
+                            var h = addZero(d.getHours());
+                            var m = addZero(d.getMinutes());
+                            var s = addZero(d.getSeconds());
+                            var t = h + ":" + m + ":" + s;
+
+                            $scope.chart1_label.push(t);
+
+                            // $scope.labels.push(data.timestamps);
+                            chart1_tempdata.push(data.value);
+                            // console.log("Timestamps : " + data.timestamps+ "\nValues : " +
+                            // data.value);
+
+                        });
+
+                        $scope.chart1_data.push(chart1_tempdata);
+                    });
+
+                }
+                
+                
+                $scope.getBarCharts = function() {
+                	
+                	
+                     $scope.chart2_data=[];
+
+                $http.get("https://energyiotbackdev.mybluemix.net/tags/gettag/" + sid1)
+                    .then(function(response) {
+
+                    	console.log("bar1 called");
+                        angular.forEach(response.data, function(data) {
+                        	
+                            var d = new Date(data.timestamps);
+                            var h = addZero(d.getHours());
+                            var m = addZero(d.getMinutes());
+                            var s = addZero(d.getSeconds());
+                            var t = h + ":" + m + ":" + s;
+
+                            // $scope.chart2_label.push(t);
+
+                            // $scope.labels.push(data.timestamps);
+                            chart2_tempdata.push(data.value);
+
+                        });
+
+                        $scope.chart2_data.push(chart2_tempdata);
+                    });
+
+                $http.get("https://energyiotbackdev.mybluemix.net/tags/gettag/" + sid2)
+                    .then(function(response) {
+                    	console.log("bar2 called");
+                        angular.forEach(response.data, function(data) {
+
+                            chart2_tempdata1.push(data.value);
+
+                        });
+
+                        $scope.chart2_data.push(chart2_tempdata1);
+                    });
+                }
+
+                var resetdate = function() {
+
+                   startDate = new Date()
+                        .getTime() - 43200000;
+                  endDate = new Date()
+                        .getTime();
+
+                }
+
+                $scope.counter = 0;
+                $scope.incrementCounter = function() {
+                    $scope.counter += 1;
+                }
+
+
+
+                $scope.refreshTag = function() {
+                    $scope.gettag15();
+                    $scope.gettag16();
+                    $scope.gettag17();
+                   }
+                var promise;
+                var promise1;
+
+
+                // starts the interval
+                $scope.start = function() {
+                    $scope.stop();
+
+                    promise = $interval($scope.refreshTag, 1000);
+                    promise1 = $interval($scope.incrementCounter, 500);
+                };
+
+                $scope.stop = function() {
+                    $interval.cancel(promise);
+                    $interval.cancel(promise1);
+                };
+
+                $scope.start();
+
+                $scope.$on('$destroy', function() {
+                    $scope.stop();
+                });
+
+                $scope.gettag15 = function() {
+
+                    resetdate();
+                    $http
+                        .get(
+                            "https://energyiotbackdev.mybluemix.net/logics/sum/" +
+                            sid +
+                            "/" +
+                           $scope.startDate +
+                            "/" +
+                            $scope.endDate)
+                        .then(
+                            function(response) {
+                              $scope.tagValue15 = response.data;
+                                
+
+                                //console.log("Avg Date is "+ $rootScope.myDateRange.tagValue15)
+                            },
+                            function(error) {
+                                // alert('Error!');
+                            });
+
+                };
+
+                $scope.gettag16 = function() {
+                    $http
+                        .get(
+                            "https://energyiotbackdev.mybluemix.net/logics/sum/16/" +
+                            $scope.startDate +
+                            "/" +
+                            $scope.endDate)
+                        .then(
+                            function(response) {
+                            	 $scope.tagValue16 = response.data;
+                                  //console.log("Avg Date is "	+ $rootScope.myDateRange.tagValue16)
+                            },
+                            function(error) {
+                                // alert('Error!');
+                            });
+
+                };
+
+                $scope.gettag17 = function() {
+                    $http
+                        .get(
+                            "https://energyiotbackdev.mybluemix.net/logics/sum/17/" +
+                            $scope.startDate +
+                            "/" +
+                            $scope.endDate)
+                        .then(
+                            function(response) {
+                            	 $scope.tagValue17 = response.data;
+                                  //console.log("Avg Date is "+ $rootScope.myDateRange.tagValue17)
+                            },
+                            function(error) {
+                                // alert('Error!');
+                            });
+
+                };
+
+            }
+
+        ]); //End of RouteController
 
 var ListController = function($scope, $http) {
 
-	var $this = this;
+    var $this = this;
 
-	$this.doSomething = function(clickedId, nameId) {
+    $this.doSomething = function(clickedId, nameId) {
 
-		console.log("Clicked Id is " + clickedId + " : " + nameId)
+        console.log("Clicked Id is " + clickedId + " : " + nameId)
 
-	};
+    };
 
 }
 
